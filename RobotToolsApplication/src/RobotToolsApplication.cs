63a1cee9afdd94dfc82fc6af2871c9cd719fa8a2
deshi-dw <deshi-dw@gmail.com>
2020-05-26 04:35:20 -0600

    Added basic application entrypoint

---
+++ RobotToolsApplication/src/RobotToolsApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace RobotTools.Application
+{
+	public static class RobotToolsApplication
+	{
+		private static void Main(string[] args)
+		{
+			string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+			PluginFinder finder = new PluginFinder($"{currentDir}\\plugins");
+			PluginLoader loader = new PluginLoader(finder.FindAll());
+
+			loader.EnableAll();
+		}
+	}
+}
