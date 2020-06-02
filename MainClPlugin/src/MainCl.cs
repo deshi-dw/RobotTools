bf62d4adae0e37e136ccc67c7777825561e1c49f
deshi-dw <deshi-dw@gmail.com>
2020-06-01 18:20:22 -0600

    Added a quit feature to MainCl

--- MainClPlugin/src/MainCl.cs
+++ MainClPlugin/src/MainCl.cs
@@ -1,13 +1,31 @@
+using System;
+using System.Threading.Tasks;
+
 namespace RobotTools.MainClPlugin
 {
 	public class MainCl : Plugin
 	{
+		private bool hasQuit = false;
+
 		public CommandExecutor Executor { get; private set; }
 
 		public override void Enable(PluginManager manager)
 		{
 			Executor = new CommandExecutor(new CommandParser());
+			Executor.Register("quit", 0, OnQuit);
+
+			Task.Run(() => {
+				while (hasQuit == false)
+				{
+					Executor.Execute(Console.ReadLine());
+				}
+			});
 		}
 		public override void Disable() { }
+
+		private void OnQuit(string[] args)
+		{
+			hasQuit = true;
+		}
 	}
 }
